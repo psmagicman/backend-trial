e953516fa045fc0f60710b655fa4e2f45f480af0
psmagicman <julienlaw1@gmail.com>
2016-07-02 08:31:04 -0700

    added function parameters for period and start range

--- assignment1/LTV_Reports.php
+++ assignment1/LTV_Reports.php
@@ -12,8 +12,14 @@ class LTV_Reports {
     * $start: date in %m-%Y format
     * $period: integer
     */
-    public function get_bookings($start, $period) {
-
+    public function get_bookings($start = null, $period = null) {
+        // $start_time = strtotime();
+        if (null === $start) {
+            $start = 1376956800; // August 2013
+        }
+        if (null === $period) {
+            $period = 12;
+        }
         $query = <<<Q
             SELECT strftime('%m-%Y', bookingsperiod.end_timestamp, 'unixepoch') AS month, COUNT(bookingsperiod.booker_id) AS total_first_bookings, 
                 SUM(bookingsperiod.bookings_made) AS total_bookings, SUM(bookingsperiod.bookingitems_turnover) AS total_turnover 
@@ -30,18 +36,14 @@ class LTV_Reports {
                 INNER JOIN bookingitems ON bookings.id = bookingitems.booking_id
                 INNER JOIN spaces ON bookingitems.booking_id = spaces.item_id
                 WHERE strftime('%s', firstbookings.end_timestamp, 'unixepoch') 
-                    BETWEEN strftime('%s', 1376956800, 'unixepoch', 'start of month') AND strftime('%s', 1376956800, 'unixepoch', '+18 months', 'start of month')
+                    BETWEEN strftime('%s', $start, 'unixepoch', 'start of month') AND strftime('%s', $start, 'unixepoch', '+$period months', 'start of month')
                 GROUP BY firstbookings.booker_id
             ) AS bookingsperiod
             GROUP BY strftime('%m-%Y', bookingsperiod.end_timestamp, 'unixepoch')
+            ORDER BY strftime('%Y', bookingsperiod.end_timestamp, 'unixepoch') ASC
 Q;
         return $this->run_query($query);
     }
-
-    private function convert_to_seconds() {
-
-    }
-
 
     private function run_query($query) {
         return $this->db->prepare($query)->run();
